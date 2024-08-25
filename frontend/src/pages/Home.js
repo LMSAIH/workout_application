500448437e101b0775a22f02be2b03a838ae962a
Axel Velasquez <yoloax11@gmail.com>
2024-08-24 17:42:36 -0700

    updated home to add the components and show the workouts

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,45 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import WorkoutLister from "../components/WorkoutLister";
+import WorkoutAdd from "../components/WorkoutAdd";
+
 const Home = () => {
-    return ( 
-        <div className = "Home">
-            <h1> Home </h1>
-        </div>
-     );
-}
- 
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+        console.log(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
+  return (
+    <div className="Home">
+      <h1> Your workouts </h1>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutLister key={workout._id} workout={workout} />
+          ))}
+        <WorkoutAdd />
+        {!workouts && (
+          <h2 className="NoWorkouts">
+            {" "}
+            This seems to be empty... Let's get to work{" "}
+            <Link to="/AddNew"> add a workout! </Link>
+          </h2>
+        )}
+      </div>
+    </div>
+  );
+};
+
 export default Home;
