c7771f373592402867588bceca525d09a829ca61
Axel Velasquez <yoloax11@gmail.com>
2024-08-26 16:52:39 -0700

    updated the usercontroller to emit json web tokens for user authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,37 @@
-const User = require('../models/userModel');
+const User = require("../models/userModel");
+const jwt = require('jsonwebtoken');
 
-const loginUser = async (req,res) =>{
-    res.json({message: "login user"});
+const createToken = (_id) => {
+   return jwt.sign({_id: _id}, process.env.SECRET, { expiresIn: '1d'});
 }
 
-const signUpUser = async (req,res) =>{
-    res.json({message: "signing user up"});
-}
+const loginUser = async (req, res) => {
 
-module.exports = {loginUser, signUpUser}
+    const { email, password } = req.body;
+
+    try{
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.status(200).json({email, token, message: 'loggedIn succesfully'});
+    } catch(err){
+        res.status(400).json({error:err.message});
+    }
+
+};
+
+const signUpUser = async (req, res) => {
+
+  const { email, password } = req.body;
+
+ 
+  try {
+    const user = await User.signup(email,password);
+    const token = createToken(user._id);
+    res.status(200).json({ email, token, message: 'signedup succefully' });
+  } catch (err) {
+    res.status(400).json({err: err.message});
+  }
+  
+};
+
+module.exports = { loginUser, signUpUser };
