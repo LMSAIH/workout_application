b81c121e91c4a147d8c1da4241a2bcfc93fb38c7
Axel Velasquez <yoloax11@gmail.com>
2024-08-27 23:47:54 -0700

    added the routes for signup and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import NavBar from './components/Nav';
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <div className = "pages"> 
           <Routes>
             <Route path = "/" element={<Home />} />
+            <Route path = "/signup" element={<SignUp />}/>
+            <Route path = '/login' element={<LogIn />} />
             <Route path= "*" element = {<NotFound />} />
           </Routes>
         </div>
