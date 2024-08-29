a569f3ea21d9a9292665c184a29fd9ceb76822c6
Axel Velasquez <yoloax11@gmail.com>
2024-08-29 00:41:40 -0700

    created the requireAuth middleware to secure the routes and check if the user is properly authorized

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,8 +1,32 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
 
-
-const requireAuth = (req,res,next) => {
+const requireAuth = async (req,res,next) => {
 
     //verify authentication
+    const { authorization } = req.headers;
 
+    if(!authorization){
+
+        return res.status(401).json({error: "Authorization token required"});
+
+    }
+
+    //getting the token at pos 1
+    const token = authorization.split(' ')[1];
+
+    try{
+        const {_id} = jwt.verify(token, process.env.SECRET);
+
+        //stores the user in the user object
+        req.user = await User.findOne({_id}).select('_id');
+        next();
+
+    }catch(error){
+        console.log(error);
+        res.status(401).json({error: 'request is not authorized'});
+    }
 
 }
+
+module.exports = requireAuth;
