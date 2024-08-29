651de56b6743a15773b8f31eb8a1d1f875253d26
Axel Velasquez <yoloax11@gmail.com>
2024-08-28 17:04:53 -0700

    changed the login page to look better and include error messages and functionalities for when it is loading

--- frontend/src/pages/LogIn.js
+++ frontend/src/pages/LogIn.js
@@ -1,18 +1,24 @@
 import { useState } from "react";
+import { useLogIn } from "../hooks/useLogIn";
+import { useNavigate } from 'react-router-dom';
 
 const LogIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isLoading } = useLogIn();
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    console.log(email, password);
+    const loggedIn = await login(email,password);
+    console.log(loggedIn);
+    navigate('/');
   };
 
   return (
     <div className="loginwrapper">
       <form className="login" onSubmit={handleSubmit}>
+        <h1> LogIn</h1>
         <label> Email </label>
         <input
           type="email"
@@ -25,7 +31,8 @@ const LogIn = () => {
           onChange={(e) => setPassword(e.target.value)}
           value={password}
         ></input>
-        <button> Log In</button>
+        <button disabled={isLoading}> Log In</button>
+        {error && <div className="LoginError">{error} </div>}
       </form>
     </div>
   );
