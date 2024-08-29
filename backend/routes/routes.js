d78f67bc099041701f2f440faba7a26b93d50a0e
Axel Velasquez <yoloax11@gmail.com>
2024-08-29 00:42:22 -0700

    changed the routes on the backend to use the authorization middleware

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const Workout = require("../models/workouts");
 const controller = require("../controllers/controller");
+const requireAuth = require('../middleware/requireAuth');
 
 //get all workouts
+
+//require the authentication for all routes
+router.use(requireAuth);
+
 router.get("/", controller.getWorkouts);
 
 //get single workout
