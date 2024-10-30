175520c2b9637864222812d7e8f50fc0cdb95a29
Axel Velasquez <yoloax11@gmail.com>
2024-10-29 20:24:39 -0700

    modified the user controller

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -8,7 +8,7 @@ const getWorkouts = async (req,res) => {
     const user_id = req.user._id;
 
 
-    const workouts = await Workout.find({user_id}).sort({createdAt: -1})
+    const workouts = await Workout.find({_id: user_id}).sort({createdAt: -1})
 
     res.status(200).json(workouts);
     
